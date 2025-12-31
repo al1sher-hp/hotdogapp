0219a40315f0ced27ff069b78f37d69263edca22
al1sher-hp <alishernusratov8@gmail.com>
2025-12-31 09:43:32 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -44,14 +44,6 @@ app.use('/api', orderRoutes);
 app.use('/api', menuRoutes);
 app.use('/api/admin', adminRoutes);
 
-// Serve static files from React app in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    });
-}
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
@@ -106,3 +98,4 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server ${PORT} portda ishlamoqda`);
 });
+
